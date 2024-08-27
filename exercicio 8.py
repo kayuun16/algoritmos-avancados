5da361b16f108ef8d94b6b005af736490abdc7c4
Kauan Lucas <klasgamer3@gmail.com>
2024-08-27 15:36:00 -0300

    Add files via upload

---
+++ exercicio 8.py
@@ -0,0 +1,33 @@
+#Qual a quantidade de votos brancos?
+#nulos?
+#válidos?
+#============================================
+#Output
+#Total de porcentagens
+
+#Total de votos válidos: x%
+#Total de votos em branco: y%
+#Total de votos nulos: t%
+
+print("Qual a quantidade de votos brancos?")
+votos_branco = int(input())
+
+print("Qual a quantidade de votos nulos?")
+votos_nulos = int(input())
+
+print("Qual a quantidade de votos validos?")
+votos_validos = int(input())
+
+votos_totais = votos_validos + votos_nulos + votos_branco
+
+validos_percentual = (votos_validos / votos_totais) * 100
+
+branco_percentual = (votos_branco / votos_totais) * 100
+
+nulos_percentual = (votos_nulos / votos_totais) * 100 
+
+print("Total de Porcentagens:")
+
+print("Total de votos validos: " + str(validos_percentual), "%")
+print("Total de votos em branco: " + str(branco_percentual), "%")
+print("Total de votos nulos: " + str(nulos_percentual), "%")
