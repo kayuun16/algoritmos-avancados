d11fa82b7f018db807dcca027a81a656a0f91b29
Kauan Lucas <klasgamer3@gmail.com>
2024-08-27 15:37:56 -0300

    exercicio 8.py

--- exercicio 8.py
+++ exercicio 8.py
@@ -1,10 +1,11 @@
+#8) Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.
+#Inputs
 #Qual a quantidade de votos brancos?
 #nulos?
 #válidos?
 #============================================
 #Output
 #Total de porcentagens
-
 #Total de votos válidos: x%
 #Total de votos em branco: y%
 #Total de votos nulos: t%
