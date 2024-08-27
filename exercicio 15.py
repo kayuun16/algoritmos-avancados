0f6335913d48e8a23e1ffbdfe75b998a31e57e48
Kauan Lucas <klasgamer3@gmail.com>
2024-08-26 22:09:41 -0300

    exercicio 15.py

---
+++ exercicio 15.py
@@ -0,0 +1,8 @@
+# 15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+print("Escreva um valor")
+valor = float(input())
+
+if valor >= 0:
+    print("Positivo")
+else:
+    print("Negativo")
