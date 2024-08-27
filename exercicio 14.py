a8b7f48e83778559e046fb7734037b491362dd4e
Kauan Lucas <klasgamer3@gmail.com>
2024-08-26 22:09:01 -0300

    exercicio 14.py

---
+++ exercicio 14.py
@@ -0,0 +1,8 @@
+# 14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+print("Digite um valor")
+valor = float(input())
+
+if valor <= 10:
+    print("Nao e maior que 10!")
+else:
+    print("E maior que 10")
