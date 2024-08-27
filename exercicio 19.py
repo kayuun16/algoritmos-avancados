f54160679c9e681211fa13a553f1d37308a41552
Kauan Lucas <klasgamer3@gmail.com>
2024-08-26 22:14:17 -0300

    exercicio 19.py

---
+++ exercicio 19.py
@@ -0,0 +1,11 @@
+# 19) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
+print("Digite um valor")
+valor_1 = float(input())
+
+print("Digite outro valor")
+valor_2 = float(input())
+
+if valor_1 > valor_2:
+    print("O maior valor e : ", valor_1)
+else:
+    print("O maior valor e : ", valor_2)
