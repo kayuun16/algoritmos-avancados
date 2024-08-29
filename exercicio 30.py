2e4d4d98e8f0fd79b0f3c887caee5c61ae3bf90a
Kauan Lucas <klasgamer3@gmail.com>
2024-08-29 11:38:29 -0300

    exercicio 30.py

--- exercicio 30.py
+++ exercicio 30.py
@@ -9,18 +9,8 @@ valor_2 = float(input())
 print("Digite mais um valor:")
 valor_3 = float(input())
 
-if valor_1 < valor_2 and valor_1 < valor_3:
-    if valor_2 < valor_3:
-        print(valor_1, valor_2, valor_3)
-    else:
-        print(valor_1, valor_3, valor_2)
-elif valor_2 < valor_1 and valor_2 < valor_3:
-    if valor_1 < valor_3:
-        print(valor_2, valor_1, valor_3)
-    else:
-        print(valor_2, valor_3, valor_1)
-else:
-    if valor_1 < valor_2:
-        print(valor_3, valor_1, valor_2)
-    else:
-        print(valor_3, valor_2, valor_1)
+valores = [valor_1, valor_2, valor_3]
+
+valores.sort()
+
+print("Valores em ordem crescente: " + str(valores))
