e0bac02b4d67fc8722afd23d8725c07bdb9d0187
Kauan Lucas <klasgamer3@gmail.com>
2024-08-28 21:09:01 -0300

    exercicio 38.py

--- exercicio 38.py
+++ exercicio 38.py
@@ -1,6 +1,7 @@
 # 38) Faça um algoritmo para ler um número que é um código de usuário. Caso este código seja diferente de um código armazenado internamente no algoritmo (igual a 1234) deve ser apresentada a mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’. Caso a senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’.
 cod_correto = 1234
 senha_correta = 9999
+senha = 0
 
 print("Digite seu codigo:")
 cod = int(input())
