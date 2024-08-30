342df92092c8289e87d59fd00c81d16c8a838a74
Kauan Lucas <klasgamer3@gmail.com>
2024-08-30 01:08:28 -0300

    exec-53.py

--- exec-53.py
+++ exec-53.py
@@ -1,8 +1,6 @@
 # 53) Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive). Considere que o N será sempre maior que ZERO
 
-N0 = int(input("Digite um valor maior que 0:"))
+N = int(input("Digite um valor maior que 0:"))
 
-N = N0 + 1
-
-for valores in range(1, N):
+for valores in range(1, N + 1):
     print(valores)
