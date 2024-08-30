964b05be2a07e78763acfed7e0699a308ba26450
Kauan Lucas <klasgamer3@gmail.com>
2024-08-30 18:18:55 -0300

    exec-62.py

--- exec-62.py
+++ exec-62.py
@@ -1,12 +1,15 @@
 # 62) Ler o número de alunos existentes em uma turma e, após isto, ler as notas destes alunos, calcular e escrever a média aritmética dessas notas lidas.
 
 soma = 0
+cont = 1
+
 total_alunos = int(input('Ha quantos alunos na turma? '))
 
-for alunos in range (1, total_alunos + 1):
-    notas = float(input(f'Qual a nota do aluno {alunos}? '))
+while cont <= total_alunos:
+    notas = float(input(f'Qual a nota do aluno {cont}? '))
     soma += notas
+    cont += 1
     
 media = soma / total_alunos
 
-print(f'Media da turma: {media:.1f}')
+print(f'Media da turma: {media:.2f}')
