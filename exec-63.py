f5d92f1adb9ed2da33fbc044a2862c6e6342639b
Kauan Lucas <klasgamer3@gmail.com>
2024-08-30 18:20:43 -0300

    exec-63.py

--- exec-63.py
+++ exec-63.py
@@ -1,9 +1,11 @@
 # 63) Escreva um algoritmo para ler 10 números e ao final da leitura escrever a soma total dos 10 números lidos.
 
 soma = 0
+cont = 1
 
-for i in range (1, 11):
+while cont <= 10:
     valores = float(input('Digite um valor: '))
     soma += valores
+    cont += 1
     
 print(f'Soma dos valores: {soma}')
