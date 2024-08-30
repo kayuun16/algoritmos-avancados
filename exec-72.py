b4a084511df2a21dd64a45d2dd0a33c420729b0b
Kauan Lucas <klasgamer3@gmail.com>
2024-08-30 19:45:09 -0300

    Add files via upload

---
+++ exec-72.py
@@ -0,0 +1,19 @@
+# 72) Faça um algoritmo para ler o código e o preço de 15 produtos, calcular e escrever: 
+#- o maior preço lido
+#- a média aritmética dos preços dos produtos
+
+soma = 0
+maior_preco = 0
+
+for i in range(1, 16):
+    cod_produto = str(input('Digite o codigo do produto: '))
+    preco_produto = float(input('Digite o preco do produto: '))
+    soma += preco_produto
+    
+    if maior_preco == 0 or maior_preco < preco_produto:
+        maior_preco = preco_produto
+        
+media = soma / 15
+
+print(f'Maior preco: R${maior_preco}')
+print(f'Media aritmetica dos precos dos produtos: {media:.2f}')
