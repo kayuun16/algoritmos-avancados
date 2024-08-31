76fbc30779b99128229af9281ce7a02c2b0b3c20
Kauan Lucas <klasgamer3@gmail.com>
2024-08-30 21:33:27 -0300

    Add files via upload

---
+++ exec-74.py
@@ -0,0 +1,7 @@
+# 74) Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10.
+
+for i in range(1, 11):
+    print(f'Tabuada do {i}: ')
+    for j in range(1, 11):
+        print(f'{i} x {j} = {i * j}')
+    print()
