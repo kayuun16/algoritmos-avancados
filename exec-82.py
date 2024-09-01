429373297aa89961a5a03b4a0ff1463ba136b752
Kauan Lucas <klasgamer3@gmail.com>
2024-09-01 20:22:52 -0300

    exec-82.py

--- exec-82.py
+++ exec-82.py
@@ -1,17 +1,17 @@
 # 82) Ler um vetor A de 10 números. Após, ler mais um número e guardar em uma variável X. Armazenar em um vetor M o resultado de cada elemento de A multiplicado pelo valor X. Logo após, imprimir o vetor M.
 
-A = [None] * 5
+A = [None] * 10
 
-for i in range(5):
+for i in range(10):
     A[i] = float(input(f'Digite um valor na posicao {i + 1} do vetor A: '))
     
 X = float(input('Digite um valor: '))
 
-M = [None] * 5
+M = [None] * 10
 
-for i in range(5):
+for i in range(10):
     M[i] = A[i] * X
     
 print('Vetor M:')
-for i in range(5):
+for i in range(10):
     print(f'{M[i]}')
