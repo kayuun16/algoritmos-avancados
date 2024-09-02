b42f491e1b9a1b6f5d6d468816aa83e0d0a8348b
Kauan Lucas <klasgamer3@gmail.com>
2024-09-02 19:22:10 -0300

    exec-91.py

--- exec-91.py
+++ exec-91.py
@@ -1,13 +1,13 @@
 # 91) Faça um algoritmo para ler 50 números e armazenar em um vetor VET, verificar e escrever se existem números repetidos no vetor VET e em que posições se encontram.
 
-vet = [None] * 10
+vet = [None] * 50
 
-for i in range(10):
+for i in range(50):
     vet[i] = float(input(f'Digite um valor na posicao {i + 1} do vetor: '))
 
 encontrou_repetidos = False
 
-for i in range(10):
+for i in range(50):
     valor = vet[i]
     
     for j in range(i):
